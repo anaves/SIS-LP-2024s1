27f615cdda5ecd590b6afd29beb3e072776cb0bf
Alysson Naves <alysson.naves@ectare.com.br>
2024-03-20 20:23:47 -0300

    aula 20-3

---
+++ sequencial/exemplo_area.py
@@ -0,0 +1,8 @@
+#  calcular a area de triangulo retangulo
+# entrada
+altura = 15
+base = 3
+# processamento
+area = (base*altura)/2
+# saida
+print(f"area: {area}")
---
+++ sequencial/exemplo_tipo.py
@@ -0,0 +1,6 @@
+A = 8
+print(type(A))
+A = 8.5
+print(type(A))
+A = "ola"
+print(type(A))
